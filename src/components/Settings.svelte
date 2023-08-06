26715617974a0d2aeaee4febe8dfe02474aad6dd
aryan02420 <aryan02420@gmail.com>
2023-08-06 00:01:09 +0000

    feat: show all colors

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -2,7 +2,7 @@
   import {
     ChevronLeft,
     ChevronRight,
-    Dices,
+    Dice5,
     Edit3,
     Eraser,
     GripVertical,
@@ -33,8 +33,8 @@
     setStrokeColorRandom,
     setStrokeColorSourceRandom,
     setStrokeColorSourceTheme,
-    setStrokeColorThemeIndexNext,
-    setStrokeColorThemeIndexPrev,
+    strokeColorThemeIndex,
+    setStrokeColorThemeIndex,
     setStrokeDisappearGroup,
     setStrokeDisappearIndependent,
     setStrokeDisappearInstant,
@@ -60,6 +60,7 @@
     setBrushTypeArrow,
   } from '../store/settings'
   import { openHelp } from '../utils/appwindow'
+  import { colors } from '../consts'
 </script>
 
 <div id="settings" class="widget">
@@ -164,13 +165,15 @@
     <div class="settings_button_group">
       <Button
         class="flex-fill"
-        onClick={setStrokeColorSourceRandom}
+        onClick={() => {
+          setStrokeColorSourceRandom()
+          setStrokeColorRandom()
+        }}
         aria-pressed={$strokeColorSource === StrokeColorSourceType.Random}
       >
-        <Button onClick={setStrokeColorRandom}>
-          <Dices size={16} strokeWidth={1.5} class="text-primary" />
-        </Button>
-        <Color color={`rgb(${$strokeColorRandom})`} />
+        <Color color={`rgb(${$strokeColorRandom})`}>
+          <Dice5 size={12} color="rgb(var(--fg)" strokeWidth={2} class="text-primary" />
+        </Color>
       </Button>
       <!--
       <Button class="flex-fill">
@@ -180,19 +183,19 @@
         <Color />
       </Button>
       -->
+      {#each colors as color, i (i)}
       <Button
         class="flex-fill"
-        onClick={setStrokeColorSourceTheme}
-        aria-pressed={$strokeColorSource === StrokeColorSourceType.Theme}
+        onClick={() => {
+          setStrokeColorSourceTheme()
+          setStrokeColorThemeIndex(i)
+        }}
+        aria-pressed={$strokeColorSource === StrokeColorSourceType.Theme && $strokeColorThemeIndex === i}
       >
-        <Button onClick={setStrokeColorThemeIndexPrev}
-          ><ChevronLeft size={16} strokeWidth={1.5} class="text-primary" /></Button
-        >
-        <Color color={`rgb(${$strokeColorTheme})`} />
-        <Button onClick={setStrokeColorThemeIndexNext}
-          ><ChevronRight size={16} strokeWidth={1.5} class="text-primary" /></Button
-        >
+        <Color color={`rgb(${color})`} />
       </Button>
+    {/each}
+
     </div>
   </div>
 </div>
@@ -201,7 +204,7 @@
   #settings {
     position: absolute;
     inset: 50%;
-    width: 360px;
+    width: 480px;
     height: min-content;
     transform: translate(-50%, -50%);
     display: flex;
@@ -223,8 +226,8 @@
     width: 100%;
     height: min-content;
   } */
-    .settings_button_group {
-      display: flex;
+  .settings_button_group {
+    display: flex;
     flex-direction: row;
     align-items: stretch;
     gap: 0px;
